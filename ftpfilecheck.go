a39adc17af386b8720ec00859fec468510770a7c
joerg kost <jk@ip-clear.de>
2018-04-23 10:53:34 +0200

    - Added sftp layer

--- ftpfilecheck.go
+++ ftpfilecheck.go
@@ -1,4 +1,4 @@
-// Copyright © 2016 Jörg Kost, joerg.kost@gmx.com
+// Copyright © 2018 Jörg Kost, joerg.kost@gmx.com
 // ftpfilecheck-plugin for nagios or icinga
 // License: https://creativecommons.org/licenses/by-nc-sa/4.0/
 
@@ -10,6 +10,9 @@ import (
 	"github.com/jlaffaye/ftp"
 	"os"
 	"time"
+	"strings"
+	"golang.org/x/crypto/ssh"
+	"github.com/pkg/sftp"
 )
 
 const (
@@ -45,13 +48,96 @@ var minSize = flag.Uint64("minsize", 1, "minimum shall be 1 byte ")
 var maxSize = flag.Uint64("maxsize", MaxUint, "maximum  Uint64 size")
 
 func main() {
+
+	flag.Parse()
+	if strings.HasSuffix(*hostPort, ":22") {
+		checkFileFromSFTP()
+	} else {
+		checkFileFromFTP()
+	}
+
+}
+
+func checkFileFromSFTP () {
+	var ftpStatus = stateFail
+	var ftpMessage = "Unbekannt"
+	var client *sftp.Client
+	var client_cwd, FilenameFull string
+	var fileInfo os.FileInfo
+
+	t := time.Now()
+	ty := t.Add(-24 * time.Hour)
+
+	config := &ssh.ClientConfig{
+		User: *login,
+		Auth: []ssh.AuthMethod{
+		ssh.Password(*password)},
+		HostKeyCallback: ssh.InsecureIgnoreHostKey(),
+	}
+
+	config.SetDefaults()
+	config.Ciphers = append(config.Ciphers, "aes128-cbc", "3des-cbc")
+
+	conn, err := ssh.Dial("tcp", *hostPort, config)
+	if err != nil {
+		ftpMessage = ftpCantConnect
+		goto printError
+		}
+
+	client, err = sftp.NewClient(conn)
+	if err != nil {
+		ftpMessage = ftpCantLogin
+		goto printError
+		}
+
+	// Close connection later
+	defer client.Close()
+	client_cwd, err = client.Getwd()
+	if err != nil {
+		ftpMessage = ftpCantCmd
+		goto printError
+	}
+	println("Current working directory:", client_cwd)
+
+	// Generate FileName
+	if *addToday == true {
+		FilenameFull = fmt.Sprintf("%s%s%02d-%02d-%02d%s", *fileName, *fileDelim, t.Year(), t.Month(), t.Day(),
+			*fileSuffix)
+	} else if *addYesterday == true {
+		FilenameFull = fmt.Sprintf("%s%s%02d-%02d-%02d%s", *fileName, *fileDelim, ty.Year(), ty.Month(), ty.Day(),
+			*fileSuffix)
+	} else {
+		FilenameFull = fmt.Sprintf("%s%s", *fileName, *fileSuffix)
+	}
+	fileInfo, err = client.Stat(*logDir+FilenameFull)
+	if err != nil {
+		ftpMessage = fmt.Sprintf(ftpCantFind, FilenameFull)
+		ftpStatus = stateFail
+		goto printError
+	}
+
+	if uint64(fileInfo.Size()) <= *minSize {
+		ftpMessage = fmt.Sprintf(ftpFileWrong, FilenameFull, fileInfo.Size())
+		ftpStatus = stateWarning
+	} else if uint64(fileInfo.Size()) > *maxSize {
+		ftpMessage = fmt.Sprintf(ftpFileWrong, FilenameFull,fileInfo.Size())
+		ftpStatus = stateWarning
+	} else {
+		ftpMessage = fmt.Sprintf(ftpCmdOk, FilenameFull, fileInfo.Size())
+		ftpStatus = stateOk
+	}
+
+printError:
+	fmt.Printf("%s %s\n", nagios[ftpStatus], ftpMessage)
+	os.Exit(ftpStatus)
+}
+
+func checkFileFromFTP () {
 	var ftpStatus = stateFail
 	var ftpMessage = "Unbekannt"
 	var FilenameFull string
 	var FileFound = false
 	var files []*ftp.Entry
-
-	flag.Parse()
 
 	t := time.Now()
 	ty := t.Add(-24 * time.Hour)
